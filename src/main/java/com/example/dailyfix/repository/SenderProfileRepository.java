f9f99029de386a7550353bc4a57cad3b8ef142ef
Venkatesh2005v <venkateshgumpina26@gmail.com>
2025-12-23 22:53:44 +0530

    created repositories

--- src/main/java/com/example/dailyfix/repository/SenderProfileRepository.java
+++ src/main/java/com/example/dailyfix/repository/SenderProfileRepository.java
@@ -2,6 +2,8 @@ package com.example.dailyfix.repository;
 
 import com.example.dailyfix.model.SenderProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SenderProfileRepository extends JpaRepository<SenderProfile, Long> {
 }
